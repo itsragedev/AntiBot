62a3ac1303ada573bbb6d0c5d7ac60cbb2fddeba
itsragedev <66060223+itsragedev@users.noreply.github.com>
2021-05-30 15:18:06 +0200

    Initial version
    Version 1.0.0

---
+++ antiBot.js
@@ -0,0 +1,28 @@
+/*
+## AntiBot 1.0.0
+## Simple human verification for your webpage
+## (C) 2021 ItsRage (github.com/itsragedev)
+*/
+
+function getCookie(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    else
+    {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+        end = dc.length;
+        }
+    }
+    // because unescape has been deprecated, replaced with decodeURI
+    //return unescape(dc.substring(begin + prefix.length, end));
+    return decodeURI(dc.substring(begin + prefix.length, end));
+}
+
+/* code based on http://stackoverflow.com/questions/5968196/ddg#5968306 */
